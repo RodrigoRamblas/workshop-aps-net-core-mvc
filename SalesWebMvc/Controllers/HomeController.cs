532c5b1b82fb41decb56d27e372cac1333a7bfe9
Rodrigo Ramblas <rodrigo_ramblas@hotmail.com>
2019-11-12 13:44:19 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course";
+            ViewData["Professor"] = "rodrigo_ramblas@hotmail.com";
 
             return View();
         }
